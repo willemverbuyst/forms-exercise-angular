55a1e35c830aadfce15d991080b8195ee165d299
Willem Verbuyst <willemverbuyst@gmail.com>
2020-09-05 12:18:46 +0200

    Replace radio buttons

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -38,17 +38,17 @@ export class UserSettingsFormComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log('in onSubmit: ', form.valid);
+    console.log('in onSubmit: ', form.value);
 
-    if (form.valid) {
-      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
-        (result) => console.log('succes: ', result),
-        (error) => this.onHttpError(error)
-      );
-    } else {
-      this.postError = true;
-      this.postErrorMessage = 'Please fix the errors above';
-    }
+    // if (form.valid) {
+    //   this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+    //     (result) => console.log('succes: ', result),
+    //     (error) => this.onHttpError(error)
+    //   );
+    // } else {
+    //   this.postError = true;
+    //   this.postErrorMessage = 'Please fix the errors above';
+    // }
   }
 
   onBlur(field: NgModel) {
