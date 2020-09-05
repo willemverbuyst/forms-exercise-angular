7f7190307b3d484b5f9f1a0c5e915d9549465c77
Willem Verbuyst <willemverbuyst@gmail.com>
2020-09-05 11:31:06 +0200

    Post form with hhtpClient and putsReq

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  postUserSettingsForm(userSettings: UserSettings): Observable<UserSettings> {
-    return of(userSettings);
+  postUserSettingsForm(userSettings: UserSettings): Observable<any> {
+    return this.http.post(
+      'https://putsreq.com/OCErEClQDSSbTd8TAYPO',
+      userSettings
+    );
+
+    // return of(userSettings);
   }
 }
